d12777ca8006d8dd3e38a84df03a109f768002dd
CAJA GARCÍA <omcagar@dsic.upv.es>
2020-01-04 15:41:15 +0100

    Implemented creator

--- P4/ProyectoAG/src/proyecto/ProyectoCreator.java
+++ P4/ProyectoAG/src/proyecto/ProyectoCreator.java
@@ -1,7 +1,17 @@
 package proyecto;
 
+import java.util.Random;
+
+import org.opt4j.core.genotype.IntegerGenotype;
 import org.opt4j.core.problem.Creator;
 
-public class ProyectoCreator implements Creator<>
-{
+public class ProyectoCreator implements Creator<IntegerGenotype> {
+	
+	public IntegerGenotype create() {
+		
+		IntegerGenotype genotipo = new IntegerGenotype(1, Datos.NUM_TURNOS);
+		genotipo.init(new Random(), Datos.NUM_CUADRILLAS);
+		
+		return genotipo;	
+	}
 }
